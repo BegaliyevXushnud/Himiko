0df2f93c3a582d0e301e61b43a20461ad821ddd9
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-09-26 11:44:34 +0500

    added about

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,35 +1,26 @@
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
-} from 'react-router-dom';
-import App from '../App';
-import { SignIn, SignUp, AdminLayout, Product,Category,Ads,Brands,BrandsCategory,Settings,Stock,Himiko } from '@pages';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 export const Index = () => {
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <>
-            <Route path="/" element={<App />}>
-            <Route index element={<SignIn />} />
-            <Route path="sign-up" element={<SignUp />} />
-                <Route path='admin-layout' element={<AdminLayout/>}>
-                
-                <Route index element={<Product/>}/>
-                <Route path='category' element={<Category/>}/>
-                <Route path='brands' element={<Brands/>}/>
-                <Route path='brands-category' element={<BrandsCategory/>}/>
-                <Route path='Stock' element={<Stock/>}/>
-                <Route path='setting' element={<Settings/>}/>
-                <Route path='ads' element={<Ads/>}/>
-                <Route path='himiko' element={<Himiko/>}/>
-                </Route>
-            </Route>
-            </>
-        )
-    );
-    return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<SignIn />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route path="admin-layout" element={<AdminLayout />}>
+            <Route index element={<Product />} />
+            <Route path="category" element={<Category />} />
+            <Route path="brands" element={<Brands />} />
+            <Route path="brands-category" element={<BrandsCategory />} />
+            <Route path="stock" element={<Stock />} />
+            <Route path="setting" element={<Settings />} />
+            <Route path="ads" element={<Ads />} />
+            <Route path="himiko" element={<Himiko />} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default Index;
